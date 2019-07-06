d187471c5ebcbb0c910f90aa09a57089df13cc87
zale <312710721@qq.com>
2019-07-06 18:09:52 +0800

    优化2019年7月6日18:09:50

--- projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MyWebSocketClient.java
+++ projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MyWebSocketClient.java
@@ -2,9 +2,14 @@ package com.bittrade.batch.socket;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.zip.Inflater;
 
+import javax.annotation.PostConstruct;
+
+import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
@@ -14,6 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.bittrade.api.service.ITParamConfigService;
+import com.bittrade.batch.enumer.ParamConfigEnum.ParamKeyEnum;
+import com.bittrade.batch.general.ObtainParamConfigInfo;
 import com.bittrade.common.utils.RedisKeyUtil;
 import com.bittrade.pojo.dto.OkexTickerDto;
 
@@ -25,10 +33,15 @@ import redis.clients.jedis.JedisCluster;
 
 public class MyWebSocketClient extends WebSocketClient {
 
-	private static final Logger	LOG	= LoggerFactory.getLogger( MyWebSocketClient.class );
+	private static final Logger		LOG					= LoggerFactory.getLogger( MyWebSocketClient.class );
 
 	@Autowired
-	private JedisCluster		jedisCluster;
+	private JedisCluster			jedisCluster;
+
+	@Autowired
+	private ITParamConfigService	paramConfigService;
+
+	private int						connectFailCount	= 1;
 
 	public MyWebSocketClient(String url) throws URISyntaxException {
 		super( new URI( url ) );
@@ -47,7 +60,8 @@ public class MyWebSocketClient extends WebSocketClient {
 	public void onMessage(String message) {
 		LOG.info( "message=" + message );
 		message = decode( message );
-		//message = "{\"table\":\"spot/ticker\",\"data\":[{\"instrument_id\":\"ETH-USDT\",\"last\":\"162.09\",\"best_bid\":\"162.1\",\"best_ask\":\"162.12\",\"open_24h\":\"165.35\",\"high_24h\":\"166.55\",\"low_24h\":\"155.69\",\"base_volume_24h\":\"620062.9352195\",\"quote_volume_24h\":\"100084770.34604578\",\"timestamp\":\"2019-04-16T10:44:06.090Z\"}]}";
+		// message =
+		// "{\"table\":\"spot/ticker\",\"data\":[{\"instrument_id\":\"ETH-USDT\",\"last\":\"162.09\",\"best_bid\":\"162.1\",\"best_ask\":\"162.12\",\"open_24h\":\"165.35\",\"high_24h\":\"166.55\",\"low_24h\":\"155.69\",\"base_volume_24h\":\"620062.9352195\",\"quote_volume_24h\":\"100084770.34604578\",\"timestamp\":\"2019-04-16T10:44:06.090Z\"}]}";
 		JSONObject jsonObject = (JSONObject) JSON.parse( message );
 		JSONArray jsonArray = (JSONArray) jsonObject.get( "data" );
 		OkexTickerDto okexTickerDto = JSONObject.parseObject( jsonArray.get( 0 ).toString(), OkexTickerDto.class );
@@ -61,6 +75,7 @@ public class MyWebSocketClient extends WebSocketClient {
 
 	@Override
 	public void onError(Exception e) {
+		// 重连操作
 		System.out.println( "异常" + e );
 	}
 
@@ -93,6 +108,40 @@ public class MyWebSocketClient extends WebSocketClient {
 			e.printStackTrace();
 		}
 		return "";
+	}
+
+	@PostConstruct
+	public void init() {
+		try {
+			LOG.info( "************WebSockeClientHandler客户端开始连接************" );
+			String[] symbols = ObtainParamConfigInfo.obtainRate( paramConfigService, ParamKeyEnum.OKEX_SYMBOL_KLINE_HISTORY_DATA_KEY.getKey() )
+					.getParamValue().split( "," );
+			StringBuffer sb = new StringBuffer( "{\"op\": \"subscribe\", \"args\":" );
+			List<String> symbolList = new ArrayList<String>();
+			for (String symbol : symbols) {
+				// 订阅
+				symbolList.add( "spot/ticker:" + symbol );
+			}
+			sb.append( JSON.toJSON( symbolList.toArray() ) );
+			sb.append( "}" );
+			String okexWebsocketUrl = ObtainParamConfigInfo.obtainRate( paramConfigService, ParamKeyEnum.OKEX_WEB_SOCKET_URL_KEY.getKey() )
+					.getParamValue();
+			// okexWebsocketUrl = "ws://127.0.0.1:7397";
+			MyWebSocketClient client = new MyWebSocketClient( okexWebsocketUrl );
+			client.connect();
+			while (!client.getReadyState().equals( WebSocket.READYSTATE.OPEN )) {
+				if (connectFailCount == 10) {
+					throw new Exception( "WebSockeClient重试次数超过10次，请排查原因..." );
+				}
+				LOG.info( "WebSockeClient第" + connectFailCount + "次连接中..." );
+				connectFailCount++;
+				Thread.sleep( 2000 );
+			}
+			LOG.info( "建立websocket连接" );
+			client.send( sb.toString() );// 开始订阅
+		} catch (Exception e) {
+			LOG.error( e.getMessage(), e );
+		}
 	}
 
 }
