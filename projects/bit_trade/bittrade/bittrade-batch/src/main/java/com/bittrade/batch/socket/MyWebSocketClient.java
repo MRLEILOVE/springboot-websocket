0fc373ce422fdf6a9970bba92417fb95a8358ace
zale <312710721@qq.com>
2019-07-08 17:39:35 +0800

    MyWebSocketClient优化2019年7月8日17:39:34

--- projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MyWebSocketClient.java
+++ projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MyWebSocketClient.java
@@ -1,147 +1,84 @@
 package com.bittrade.batch.socket;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.zip.Inflater;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
-import org.java_websocket.WebSocket;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.compress.compressors.deflate64.Deflate64CompressorInputStream;
+import org.springframework.stereotype.Component;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.bittrade.api.service.ITParamConfigService;
-import com.bittrade.batch.dao.ITParamConfigDAO;
-import com.bittrade.batch.enumer.ParamConfigEnum.ParamKeyEnum;
-import com.bittrade.batch.general.ObtainParamConfigInfo;
-import com.bittrade.common.utils.RedisKeyUtil;
-import com.bittrade.pojo.dto.OkexTickerDto;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.Unpooled;
-import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
-import redis.clients.jedis.JedisCluster;
+@Component
+public class MyWebSocketClient {
 
-public class MyWebSocketClient extends WebSocketClient {
-
-	private static final Logger		LOG					= LoggerFactory.getLogger( MyWebSocketClient.class );
-
-	@Autowired
-	private JedisCluster			jedisCluster;
-
-	@Autowired
-	private ITParamConfigService<ITParamConfigDAO>	paramConfigService;
-
-	private int						connectFailCount	= 1;
-
-	public MyWebSocketClient(String url) throws URISyntaxException {
-		super( new URI( url ) );
-	}
-
-	@Override
-	public void onOpen(ServerHandshake handshakedata) {
-		System.out.println( "建立握手成功..." );
-		for (Iterator<String> it = handshakedata.iterateHttpFields(); it.hasNext();) {
-			String key = it.next();
-			System.out.println( key + ":" + handshakedata.getFieldValue( key ) );
-		}
-	}
-
-	@Override
-	public void onMessage(String message) {
-		LOG.info( "message=" + message );
-		message = decode( message );
-		// message =
-		// "{\"table\":\"spot/ticker\",\"data\":[{\"instrument_id\":\"ETH-USDT\",\"last\":\"162.09\",\"best_bid\":\"162.1\",\"best_ask\":\"162.12\",\"open_24h\":\"165.35\",\"high_24h\":\"166.55\",\"low_24h\":\"155.69\",\"base_volume_24h\":\"620062.9352195\",\"quote_volume_24h\":\"100084770.34604578\",\"timestamp\":\"2019-04-16T10:44:06.090Z\"}]}";
-		JSONObject jsonObject = (JSONObject) JSON.parse( message );
-		JSONArray jsonArray = (JSONArray) jsonObject.get( "data" );
-		OkexTickerDto okexTickerDto = JSONObject.parseObject( jsonArray.get( 0 ).toString(), OkexTickerDto.class );
-		jedisCluster.set( RedisKeyUtil.getOkexSymbolLast( okexTickerDto.getInstrument_id() ), String.valueOf( okexTickerDto.getLast() ) );
-	}
-
-	@Override
-	public void onClose(int code, String reason, boolean remote) {
-		System.out.println( "关闭..." );
-	}
-
-	@Override
-	public void onError(Exception e) {
-		// 重连操作
-		System.out.println( "异常" + e );
-	}
-
-	private static String decode(Object msg) {
-		BinaryWebSocketFrame frameBinary = (BinaryWebSocketFrame) msg;
-		byte[] bytes = new byte[frameBinary.content().readableBytes()];
-		frameBinary.content().readBytes( bytes );
-		ByteBuf byteBuf = Unpooled.wrappedBuffer( bytes );
-		String str = uncompress( byteBuf );
-		return str;
-	}
-
-	private static String uncompress(ByteBuf buf) {
-		try {
-			byte[] temp = new byte[buf.readableBytes()];
-			ByteBufInputStream bis = new ByteBufInputStream( buf );
-			bis.read( temp );
-			bis.close();
-			Inflater decompresser = new Inflater( true );
-			decompresser.setInput( temp, 0, temp.length );
-			StringBuilder sb = new StringBuilder();
-			byte[] result = new byte[1024];
-			while (!decompresser.finished()) {
-				int resultLength = decompresser.inflate( result );
-				sb.append( new String( result, 0, resultLength, "UTF-8" ) );
-			}
-			decompresser.end();
-			return sb.toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
-
-	//@PostConstruct
+	@PostConstruct
 	public void init() {
 		try {
-			LOG.info( "************WebSockeClientHandler客户端开始连接************" );
-			String[] symbols = ObtainParamConfigInfo.obtainRate( paramConfigService, ParamKeyEnum.OKEX_SYMBOL_KLINE_HISTORY_DATA_KEY.getKey() )
-					.getParamValue().split( "," );
-			StringBuffer sb = new StringBuffer( "{\"op\": \"subscribe\", \"args\":" );
-			List<String> symbolList = new ArrayList<String>();
-			for (String symbol : symbols) {
-				// 订阅
-				symbolList.add( "spot/ticker:" + symbol );
-			}
-			sb.append( JSON.toJSON( symbolList.toArray() ) );
-			sb.append( "}" );
-			String okexWebsocketUrl = ObtainParamConfigInfo.obtainRate( paramConfigService, ParamKeyEnum.OKEX_WEB_SOCKET_URL_KEY.getKey() )
-					.getParamValue();
-			// okexWebsocketUrl = "ws://127.0.0.1:7397";
-			MyWebSocketClient client = new MyWebSocketClient( okexWebsocketUrl );
-			client.connect();
-			while (!client.getReadyState().equals( WebSocket.READYSTATE.OPEN )) {
-				if (connectFailCount == 10) {
-					throw new Exception( "WebSockeClient重试次数超过10次，请排查原因..." );
+			System.out.println( "*********************************" );
+			OkHttpClient client = new OkHttpClient();
+
+			Request request = new Request.Builder().url( "wss://real.okex.com:10442/ws/v3" ).build();
+			// Request request = new
+			// Request.Builder().url("ws://127.0.0.1:7397").build();
+
+			client.newWebSocket( request, new WebSocketListener() {
+
+				@Override
+				public void onOpen(WebSocket webSocket, Response response) {
+					webSocket.send( "{\"op\": \"subscribe\", \"args\": [\"spot/ticker:ETH-USDT\",\"spot/ticker:BTC-USDT\"]}" );
 				}
-				LOG.info( "WebSockeClient第" + connectFailCount + "次连接中..." );
-				connectFailCount++;
-				Thread.sleep( 2000 );
-			}
-			LOG.info( "建立websocket连接" );
-			client.send( sb.toString() );// 开始订阅
+
+				@Override
+				public void onMessage(WebSocket webSocket, String text) {
+					System.out.println( text );
+				}
+
+				@Override
+				public void onMessage(WebSocket webSocket, ByteString bytes) {
+					System.out.println( "bytes.toByteArray()=" + bytes.toByteArray() );
+					System.out.println( "uncompress( bytes.toByteArray() )=" + uncompress( bytes.toByteArray() ) );
+				}
+
+				@Override
+				public void onClosing(WebSocket webSocket, int code, String reason) {
+					System.out.println( webSocket );
+				}
+
+				@Override
+				public void onClosed(WebSocket webSocket, int code, String reason) {
+					System.out.println( webSocket );
+				}
+
+				@Override
+				public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+					System.out.println( webSocket );
+				}
+			} );
 		} catch (Exception e) {
-			LOG.error( e.getMessage(), e );
+			System.out.println( e );
+		}
+	}
+
+	private static String uncompress(byte[] bytes) {
+		try (final ByteArrayOutputStream out = new ByteArrayOutputStream();
+				final ByteArrayInputStream in = new ByteArrayInputStream( bytes );
+				final Deflate64CompressorInputStream zin = new Deflate64CompressorInputStream( in )) {
+			final byte[] buffer = new byte[1024];
+			int offset;
+			while (-1 != (offset = zin.read( buffer ))) {
+				out.write( buffer, 0, offset );
+			}
+			return out.toString();
+		} catch (final IOException e) {
+			throw new RuntimeException( e );
 		}
 	}
 
