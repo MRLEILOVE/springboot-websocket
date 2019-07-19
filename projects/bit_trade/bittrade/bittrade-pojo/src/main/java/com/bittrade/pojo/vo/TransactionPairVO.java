d8a22acb937f105021440d2e685c1db5972529ed
dtid_d49a4a0100d731b2 <zehan15919333183@sina.com>
2019-07-19 15:47:33 +0800

    1

--- projects/bit_trade/bittrade/bittrade-pojo/src/main/java/com/bittrade/pojo/vo/TransactionPairVO.java
+++ projects/bit_trade/bittrade/bittrade-pojo/src/main/java/com/bittrade/pojo/vo/TransactionPairVO.java
@@ -1,30 +1,32 @@
 package com.bittrade.pojo.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-//@ApiModel(description = "交易对列表")
+@ApiModel(description = "交易对列表")
 public class TransactionPairVO {
-//    @ApiModelProperty(value = "id")
+    @ApiModelProperty(value = "id")
     private Integer id;
 
-//    @ApiModelProperty(value = "交易对,如BTC/USDT")
+    @ApiModelProperty(value = "交易对,如BTC/USDT")
     private String symbol;
 
-//    @ApiModelProperty(value = "货币id，t_currency表中的id")
+    @ApiModelProperty(value = "货币id，t_currency表中的id")
     private Integer currencyId1;
 
-//    @ApiModelProperty(value = "法币id，t_currency表中的id")
+    @ApiModelProperty(value = "法币id，t_currency表中的id")
     private Integer currencyId2;
 
-//    @ApiModelProperty(value = "价格")
+    @ApiModelProperty(value = "价格")
     private String price;
 
-//    @ApiModelProperty(value = "涨跌幅")
+    @ApiModelProperty(value = "涨跌幅")
     private String chg;
 
-    //    @ApiModelProperty(value = "自选id(根据有无id没判断是否加入自选)")
+    @ApiModelProperty(value = "自选id(根据有无id没判断是否加入自选)")
     private Integer optionalId;
 }
