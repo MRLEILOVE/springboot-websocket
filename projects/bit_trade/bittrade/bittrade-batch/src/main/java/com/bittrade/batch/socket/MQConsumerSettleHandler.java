2a989f008a924f5468f6816c0d8588aa420859a4
zale <904458528@qq.com>
2019-07-24 11:38:29 +0800

    优化

--- projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MQConsumerSettleHandler.java
+++ projects/bit_trade/bittrade/bittrade-batch/src/main/java/com/bittrade/batch/socket/MQConsumerSettleHandler.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.dubbo.config.annotation.Reference;
@@ -41,116 +40,5 @@ public class MQConsumerSettleHandler {
 			LOG.error( e.getMessage(), e );
 		}
 	}
-	
-
-	// private void walletSettle(TEntrustRecord entrustRecords) throws Exception
-	// {
-	// long currentUserId = entrustRecords.getUserId();// 用户id
-	// long rivalUserId = entrustRecords.getRivalUserId();// 对手方用户id
-	// long entrustRecordId = entrustRecords.getId();
-	// int currencyTradeId = entrustRecords.getCurrencyTradeId();
-	//
-	// BigDecimal count = entrustRecords.getCount();
-	// BigDecimal amount = entrustRecords.getAmount();
-	//
-	// // 2、根据交易对Id获去币种id
-	// TCurrencyTrade currencyTrade = new TCurrencyTrade();
-	// currencyTrade.setId( currencyTradeId );
-	// currencyTrade = currencyTradeService.getBy( currencyTrade );
-	// int currencyId = currencyTrade.getCurrencyId1();// 货比id
-	// int marketId = currencyTrade.getCurrencyId2();// 法币id
-	//
-	// // 3、获取卖家币币钱包信息
-	// // currentUserId-->卖currencyId，currencyId(count)↓，marketId(amout)↑
-	// TWallet sellCurrencyIdWallet = GeneralMethod.qryUserWallet(
-	// walletService, currentUserId, currencyId );
-	// TWallet sellMarketIdWallet = GeneralMethod.qryUserWallet( walletService,
-	// currentUserId, marketId );
-	// if (sellCurrencyIdWallet.getTradeFrozen().compareTo( count ) < 0) {
-	// throw new Exception( "TEntrustRecord.id=" + entrustRecords.getId() +
-	// "sellte.faile,reason.userId=" + currentUserId + "currencyId="
-	// + currencyId + "tradeFrozen=" + sellCurrencyIdWallet.getTradeFrozen() +
-	// "count=" + count );
-	// }
-	//
-	// // 4、更新卖家币币钱包
-	// updateUserWallet( sellCurrencyIdWallet, count, entrustRecordId, true );//
-	// 减count
-	// updateUserWallet( sellMarketIdWallet, amount, entrustRecordId, false );//
-	// 加amount
-	//
-	// // 5、获取对手的币币钱包信息
-	// // rivalUserId-->买currencyId，currencyId(count)↑，marketId(amout)↓
-	// TWallet buyMarketIdWallet = GeneralMethod.qryUserWallet( walletService,
-	// rivalUserId, marketId );
-	// TWallet buyCurrencyIdWallet = GeneralMethod.qryUserWallet( walletService,
-	// rivalUserId, currencyId );
-	// if (buyMarketIdWallet.getTradeFrozen().compareTo( amount ) < 0) {
-	// throw new Exception( "TEntrustRecord.id=" + entrustRecords.getId() +
-	// "sellte.faile,reason.userId=" + rivalUserId + "marketId=" + marketId
-	// + "tradeFrozen=" + buyMarketIdWallet.getTradeFrozen() + "amount=" +
-	// amount );
-	// }
-	//
-	// // 6、更新对手钱包信息
-	// updateUserWallet( buyMarketIdWallet, amount, entrustRecordId, true );//
-	// 减amount
-	// updateUserWallet( buyCurrencyIdWallet, count, entrustRecordId, false );//
-	// 加count
-	// }
-
-	/**
-	 * updateUserWallet:(操作用户钱包). <br/>
-	 * 
-	 * @author Administrator
-	 * @param wallet
-	 * @param val
-	 * @param entrustRecordId
-	 * @param bool
-	 * @since JDK 1.8
-	 */
-	// private void updateUserWallet(TWallet wallet, BigDecimal val, long
-	// entrustRecordId, boolean bool) throws Exception {
-	// TWallet updateWallet = new TWallet();// 修改的对象
-	// if (bool) {
-	// updateWallet.setTradeFrozen( wallet.getTradeFrozen().subtract( val ) );
-	// } else {
-	// updateWallet.setTotal( wallet.getTotal().add( val ) );
-	// }
-	// updateWallet.setVersion( wallet.getVersion() + 1 );
-	// updateWallet.setUpdateTime( new Date() );
-	//
-	// TWallet updateSellMarketIdWallet = new TWallet(); // 条件
-	// updateSellMarketIdWallet.setId( wallet.getId() );
-	// updateSellMarketIdWallet.setUserId( wallet.getUserId() );
-	// updateSellMarketIdWallet.setCurrencyId( wallet.getCurrencyId() );
-	// updateSellMarketIdWallet.setVersion( wallet.getVersion() );
-	//
-	// // 更新钱包
-	// // int row = walletService.modifyWithSelectiveBy( updateWallet,
-	// // updateSellMarketIdWallet );
-	// // LOG.info( "row=" + row );
-	//
-	// // 记录钱包流水
-	// TWalletRecord walletRecord = new TWalletRecord();
-	// walletRecord.setId( SNOW_FLAKE.nextId() );
-	// walletRecord.setUserId( wallet.getUserId() );
-	// walletRecord.setCurrencyId( wallet.getCurrencyId() );
-	// walletRecord.setType( (byte) 3 );
-	// walletRecord.setEntrustRecordId( entrustRecordId );
-	// walletRecord.setCreateTime( new Date() );
-	//
-	// BigDecimal beforeAmount = wallet.getTotal().add( wallet.getTradeFrozen()
-	// ).add( wallet.getTransferFrozen() );
-	// walletRecord.setBeforeAmount( beforeAmount );
-	// if (bool) {
-	// walletRecord.setChangeAmount( BigDecimal.ZERO.subtract( val ) );
-	// } else {
-	// walletRecord.setChangeAmount( val );
-	// }
-	// walletRecord.setAfterAmount( beforeAmount.add( val ) );
-	// // 插入流水
-	// walletRecordService.add( walletRecord );
-	// }
 
 }
