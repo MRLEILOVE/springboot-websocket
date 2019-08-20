568844abf990280cec76d51b8df2ca3013037fc0
dtid_d49a4a0100d731b2 <zehan15919333183@sina.com>
2019-08-20 20:21:45 +0800

    1

--- projects/bit_trade/wallet/wallet-biz/src/main/java/com/wallet/biz/service/ICPersonalAccountService.java
+++
@@ -1,7 +0,0 @@
-package com.wallet.biz.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wallet.biz.pojo.CPersonalAccount;
-
-public interface ICPersonalAccountService extends IService<CPersonalAccount> {
-}
