2eb5b1a10c7d2821318192451663259ca78b9171
dtid_d49a4a0100d731b2 <zehan15919333183@sina.com>
2019-08-22 11:42:32 +0800

    1

--- projects/bit_trade/wallet/wallet-biz/src/main/java/com/wallet/biz/service/impl/WWalletAccountServiceImpl.java
+++ projects/bit_trade/wallet/wallet-biz/src/main/java/com/wallet/biz/service/impl/WWalletAccountServiceImpl.java
@@ -215,7 +215,7 @@ public class WWalletAccountServiceImpl extends ServiceImpl<IWWalletAccountDAO, W
      * @param userId
      */
     private void createAllWallet(List<TCurrency> currencies, Long userId) {
-        if(currencies != null || currencies.size() >= 0){
+        if(currencies != null && currencies.size() > 0){
             currencies.forEach(x ->{
                 //是否在枚举中，币币账户跟资金账户公用了一个币种表，但是资金账户现在只有BTC跟USDT
                 if(FundCoinEnumer.getValueByKey(x.getId()) != null){
