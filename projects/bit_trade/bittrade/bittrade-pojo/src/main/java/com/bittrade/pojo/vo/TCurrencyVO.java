5c3a6cdea917a22fd257cd33572d0bead16bb63e
dtid_d49a4a0100d731b2 <zehan15919333183@sina.com>
2019-08-27 11:02:40 +0800

    1

--- projects/bit_trade/bittrade/bittrade-pojo/src/main/java/com/bittrade/pojo/vo/TCurrencyVO.java
+++ projects/bit_trade/bittrade/bittrade-pojo/src/main/java/com/bittrade/pojo/vo/TCurrencyVO.java
@@ -21,8 +21,6 @@ public class TCurrencyVO extends BaseVO<TCurrencyVO> {
 	private String shortName;
 	private Byte status;
 	private Byte type;
-	private Byte isRecharge;
-	private Byte isWithdraw;
 	private String desc;
 	private java.time.LocalDateTime createTime;
 
