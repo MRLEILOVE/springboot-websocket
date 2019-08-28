ed320cb07dc82c57fffc077bcf9e037ca07367c6
肖子超 <463524785@qq.com>
2019-08-28 22:24:55 +0800

    增加uac模块

--- projects/bit_trade/bittrade/bittrade-uac/src/main/java/com/bittrade/uac/UacApplication.java
+++ projects/bit_trade/bittrade/bittrade-uac/src/main/java/com/bittrade/uac/UacApplication.java
@@ -1,8 +1,8 @@
 package com.bittrade.uac;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.core.framework.BaseApplication;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -17,8 +17,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @MapperScan("com.bittrade.uac.mapper")
 @SpringBootApplication
-public class UacApplication {
+public class UacApplication extends BaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UacApplication.class, args);
+        run(UacApplication.class, args);
     }
 }
