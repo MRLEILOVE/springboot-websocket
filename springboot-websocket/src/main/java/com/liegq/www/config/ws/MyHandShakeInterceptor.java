f23c81b281749a98fe9958378ad6e2b6e5f49761
leiguoqing <191789784@qq.com>
2019-10-13 19:11:27 +0800

    完善代码及注释

--- springboot-websocket/src/main/java/com/liegq/www/config/ws/MyHandShakeInterceptor.java
+++ springboot-websocket/src/main/java/com/liegq/www/config/ws/MyHandShakeInterceptor.java
@@ -1,4 +1,4 @@
-package com.liegq.www.config.websocket;
+package com.liegq.www.config.ws;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
--- springboot-websocket/src/main/java/com/liegq/www/config/ws/WebSocketConfig.java
+++ springboot-websocket/src/main/java/com/liegq/www/config/ws/WebSocketConfig.java
@@ -1,8 +1,7 @@
-package com.liegq.www.config.websocket;
+package com.liegq.www.config.ws;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
